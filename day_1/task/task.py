b74f254a7903091a097b6a91a1a5f761c61f7930
FlowJoe <joe.unwin@outlook.com>
2024-12-08 23:02:00 -0800

    Initial day

---
+++ day_1/task/task.py
@@ -0,0 +1,13 @@
+# Write your code below this line 👇
+# name = input("What is your name?")
+
+# print("Hello " + name + "!")
+# print(len(name))
+
+glass1 = "milk"
+glass2 = "juice"
+
+i = glass1
+glass1 = glass2
+glass2 = i
+print(glass1 + " " + glass2)
